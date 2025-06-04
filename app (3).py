02bcaf66c83778221a9865cae8f76460a92c1898
MarcoLombardo01 <lombardomarco89@yahoo.it>
2025-06-04 20:32:56 +0200

    Update app (3).py

--- app (3).py
+++ app (3).py
@@ -3,8 +3,8 @@ import streamlit as st
 from PIL import Image
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain_community.chat_models import ChatOpenAI
 import base64
